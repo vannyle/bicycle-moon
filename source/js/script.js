f0c01e79720a925c842d390a69f5005e7120538e
vannnyle@gmail.com <vannnyle@gmail.com>
2021-02-06 23:32:43 +0200

    Добавляет респонсив мобильной версии. Доработки по мобильному меню.

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navMain = document.querySelector('.header__nav');
   const navToggle = document.querySelector('.header__toggle');
+  const header = document.querySelector('.header');
 
+  header.classList.remove('header--nojs');
   navToggle.classList.remove('header__toggle--nojs');
   navMain.classList.remove('header__nav--nojs');
 
@@ -10,12 +12,16 @@ document.addEventListener("DOMContentLoaded", function () {
       navMain.classList.remove('header__nav--close');
       navMain.classList.add('header__nav--open');
       navToggle.classList.remove('header__toggle--close');
-      navToggle.classList.add('header__toggle--open')
+      navToggle.classList.add('header__toggle--open');
+      document.body.style.overflow = 'hidden';
+      document.body.style.position = 'fixed';
     } else {
       navMain.classList.remove('header__nav--open');
       navMain.classList.add('header__nav--close');
       navToggle.classList.remove('header__toggle--open');
-      navToggle.classList.add('header__toggle--close')
+      navToggle.classList.add('header__toggle--close');
+      document.body.style.overflow = 'unset';
+      document.body.style.position = 'relative';
     }
   });
 });
