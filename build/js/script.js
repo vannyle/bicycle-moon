899907712acc36faeae8f371770061b397065d5c
vannnyle@gmail.com <vannnyle@gmail.com>
2021-02-07 21:28:15 +0200

    Рефакторинг скрипта для мобильного меню.

--- build/js/script.js
+++ build/js/script.js
@@ -7,21 +7,45 @@ document.addEventListener("DOMContentLoaded", function () {
   navToggle.classList.remove('header__toggle--nojs');
   navMain.classList.remove('header__nav--nojs');
 
+  function openMenu() {
+    navMain.classList.remove('header__nav--close');
+    navMain.classList.add('header__nav--open');
+    navToggle.classList.remove('header__toggle--close');
+    navToggle.classList.add('header__toggle--open');
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+  }
+
+  function closeMenu() {
+    navMain.classList.remove('header__nav--open');
+    navMain.classList.add('header__nav--close');
+    navToggle.classList.remove('header__toggle--open');
+    navToggle.classList.add('header__toggle--close');
+    document.body.style.overflow = 'unset';
+    document.body.style.position = 'relative';
+  }
+
   navToggle.addEventListener('click', function () {
     if (navMain.classList.contains('header__nav--close')) {
-      navMain.classList.remove('header__nav--close');
-      navMain.classList.add('header__nav--open');
-      navToggle.classList.remove('header__toggle--close');
-      navToggle.classList.add('header__toggle--open');
-      document.body.style.overflow = 'hidden';
-      document.body.style.position = 'fixed';
+      openMenu();
     } else {
-      navMain.classList.remove('header__nav--open');
-      navMain.classList.add('header__nav--close');
-      navToggle.classList.remove('header__toggle--open');
-      navToggle.classList.add('header__toggle--close');
-      document.body.style.overflow = 'unset';
-      document.body.style.position = 'relative';
+      closeMenu();
+    }
+  });
+
+  header.addEventListener('click', (e) => {
+    let headerItem = e.target.closest('.header__item');
+    if (headerItem) {
+      closeMenu();
+      const link = headerItem.querySelector('a[href^="#section"]');
+      let ref = link.href.split('#section');
+      ref = "#section" + ref[1];
+      window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        // top gets the distance from the top of the page of our target element
+        top: document.querySelector(ref).offsetTop
+      });
     }
   });
 });
